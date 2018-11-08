851252ded2f7352ae47407fb8a5c3a87c18d2ae5
mao <maojunhui5214@163.com>
2018-11-08 23:22:20 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,39 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// GET请求路由
+Route::get('/get',function(){
+    return "Hello Laravel Route::get()";
+});
+
+// POST请求路由
+Route::get('/testPost',function(){
+    $csrf_token = csrf_token();
+    $form = <<<FORM
+<form action="/hello' method="POST">
+    <input type="hidden" name="_token" value="{$csrf_token}">
+    <input type="submit" value="Test"/>
+</form>
+FORM;
+    return $form;
+});
+Route::post('/hello',function(){
+    return "Hello Laravel Route::post()";
+});
+
+// 其他请求路由
+/**
+Route::match(['get','post'],'/match',function(){
+    return "Hello Laravel Route::match()";
+});
+Route::any('/any',function(){
+    return "Hello Laravel Route::any()";
+});
+ */
+// 路由参数使用
+// 必须参数
+Route::get('hello/{name}',function($name){
+    return "Hello {$name}!";
+});
+
