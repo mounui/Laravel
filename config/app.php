765674799c3d4ff64784d0ce574f4055b1bcb6cd
epins <2401052316@qq.com>
2018-11-09 12:27:53 +0800

    config edit

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:r60C6Mi/5mJ8xmf/aACIcU0trhlS5rA/sOMWzYfo+yI='),
 
     'cipher' => 'AES-256-CBC',
 
